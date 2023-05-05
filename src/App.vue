6bc9c1d4f37ed93126a8f8afe2262b425f0d3ea3
AdnanZe <adnan.zecheria@yahoo.com>
2023-05-05 18:27:29 +0300

    [wip] header component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HelloWorld from "@/components/HelloWorld.vue";
+import TheHeader from "./components/TheHeader.vue";
 </script>
 
 <template>
   <v-app>
     <v-main>
+      <TheHeader />
       <HelloWorld />
     </v-main>
   </v-app>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <v-app-bar :elevation="2"></v-app-bar>
+</template>
